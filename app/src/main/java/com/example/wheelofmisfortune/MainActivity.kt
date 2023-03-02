3163bde329091e14ff2bef5474eefcc91af8cf97
Racim-Fethallah <RacimFethallah@proton.me>
2023-03-02 14:03:07 +0100

    Wheel of misfortune base app

--- app/src/main/java/com/example/wheelofmisfortune/MainActivity.kt
+++ app/src/main/java/com/example/wheelofmisfortune/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,26 +21,21 @@ class MainActivity : AppCompatActivity() {
 
 
         val spinnerData = arrayOf("Groupe 1", "Groupe 2", "Groupe 3", "Groupe 4")
-        val spinnerAdapter = ArrayAdapter<String>(this, R.layout.spinner_item, spinnerData)
-        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val spinnerAdapter = ArrayAdapter(this, R.layout.spinner_item, spinnerData)
         mySpinner.adapter = spinnerAdapter
 
 
 
         myButton.setOnClickListener{
-            val selectedOption = mySpinner.selectedItem as String
-            if (selectedOption == "Groupe 1"){
-                val Intent = Intent(this,ActivityG1::class.java)
-                startActivity(Intent)
-            }else if (selectedOption == "Groupe 2"){
-            val Intent = Intent(this,ActivityG2::class.java)
-            startActivity(Intent)
-            }else if(selectedOption == "Groupe 3"){
-                val Intent = Intent(this,ActivityG3::class.java)
-                startActivity(Intent)
-            }else{
-                val Intent = Intent(this,ActivityG4::class.java)
-                startActivity(Intent)
+            when (mySpinner.selectedItem as String){
+                "Groupe 1" -> {val intent = Intent(this,ActivityG1::class.java)
+                    startActivity(intent)}
+                "Groupe 2" -> {val intent = Intent(this,ActivityG2::class.java)
+                    startActivity(intent)}
+                "Groupe 3" -> {val intent = Intent(this,ActivityG3::class.java)
+                    startActivity(intent)}
+                "Groupe 4" -> {val intent = Intent(this,ActivityG4::class.java)
+                    startActivity(intent)}
             }
 
         }
